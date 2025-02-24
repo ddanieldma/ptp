a69b1be99468c68e00d03fba563255d8678beac7
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2025-02-24 16:30:06 -0300

    first plot third question

--- brazil-aligned/utils.py
+++ brazil-aligned/utils.py
@@ -1,2 +1,9 @@
+import pandas as pd
+from pathlib import Path
+
 def print_equals(num: int) -> None:
     print(num*"=")
+
+def write_to_excel(df: pd.DataFrame, file_path: Path | str, sheet_name: str) -> None:
+    with pd.ExcelWriter(file_path, engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
